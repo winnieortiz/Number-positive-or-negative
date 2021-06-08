7ebe7d68ca392420dbfe519a4216db88af4dd118
marow <marow@LAPTOP-KSHTVLBC>
2021-06-08 20:52:20 +1200

    Add project files.

---
+++ Number positive or negative/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace number_positive_or_negative
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1;
+            
+            Console.WriteLine("Please enter a number");
+            number1 = Convert.ToInt32(Console.ReadLine());
+
+            if(number1 >= 0)
+            {
+                Console.WriteLine("number is positive");
+            }
+            else
+            {
+                Console.WriteLine("number is negative");
+
+            }
+        }
+
+    }
+}
